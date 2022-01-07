8bd156c303e31ea75f30382106ff90bb2564fb6e
Pranshu Jha <58560660+pranshuj73@users.noreply.github.com>
2022-01-08 00:41:25 +0530

    created ver1.0

--- pages/components/Contact.tsx
+++ pages/components/Contact.tsx
@@ -4,13 +4,13 @@ const Contact: NextPage = () => {
   return (
     <>
       <h2>Contact</h2>
-      <p>Email: <a href="mailto:pranshuj9801@gmail.com" target="_blank">pranshuj9801@gmail.com</a></p>
+      <p>Email: <a href="mailto:pranshuj9801@gmail.com" target="_blank" rel="noreferrer">pranshuj9801@gmail.com</a></p>
       <br />
-      <p>Twitter: <a href="https://twitter.com/pranshuj73" target="_blank">@pranshuj73</a></p>
+      <p>Twitter: <a href="https://twitter.com/pranshuj73" target="_blank" rel="noreferrer">@pranshuj73</a></p>
       <br />
-      <p>LinkedIn: <a href="https://www.linkedin.com/in/pranshu-jha-7ba383183/" target="_blank">Pranshu Jha</a></p>
+      <p>LinkedIn: <a href="https://www.linkedin.com/in/pranshu-jha-7ba383183/" target="_blank" rel="noreferrer">Pranshu Jha</a></p>
       <br />
-      <p>Blog: <a href="https://pranshujha.hashnode.dev/" target="_blank">pranshujha.hashnode.dev</a></p>
+      <p>Blog: <a href="https://pranshujha.hashnode.dev/" target="_blank" rel="noreferrer">pranshujha.hashnode.dev</a></p>
       <br />
     </>
   )
